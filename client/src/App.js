6fcab9d4ca4663757458b97303514529c39bea83
David Eliason <david@davethemaker.com>
2018-04-03 12:14:08 -0700

    create placeholder clickHandler function to set new state within App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
       super(props);
-      this.state = {clientData: "blue"}
+      this.state = {clientData: "blue"};
+      this.handleClick = this.handleClick.bind(this);
     }
-    
+
+   handleClick() {
+    this.setState(prevState => ({
+      clientData: prevState.clientData + "+"
+    }));
+  }
+
   render() {
     return (
       <div className="App">
         <p>IoT Empowering Me</p>
-        <p>{this.state.clientData}</p>
+        
+         <button onClick={this.handleClick}>
+          {this.state.clientData}
+         </button>
       </div>
     );
   }
