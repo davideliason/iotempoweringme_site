1b8fc8d667f2121839417517a2f7240195edc4a4
David Eliason <david@davethemaker.com>
2018-04-03 11:53:26 -0700

    add sample state value to App component

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {clientData: "blue"}
+    }
+    
   render() {
     return (
       <div className="App">
         <p>IoT Empowering Me</p>
+        <p>{this.state.clientData}</p>
       </div>
     );
   }
