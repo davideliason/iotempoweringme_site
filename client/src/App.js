555a34b31d3e3de170b541dbc897ea3b4c432929
David Eliason <david@davethemaker.com>
2018-04-18 11:59:20 -0700

    refactoring to basics for setting up state foundation

--- client/src/App.js
+++ client/src/App.js
@@ -5,73 +5,44 @@ import PubNubReact from 'pubnub-react';
 class App extends Component {
   constructor(props) {
         super(props);
+
         this.state = {
-            msgsGPSCoordinates : [],
-            msgsMessages : [],
-            msgsTimestamps : []
+            Messages : [],
         };
 
-        this.publishMessageToChannel = this.publishMessageToChannel.bind(this);
+        // this.publishMessageToChannel = this.publishMessageToChannel.bind(this);
 
         this.pubnub = new PubNubReact({
             publishKey: 'pub-c-c377ebaa-f828-40f5-8b64-9fef4ff4aeaa',
             subscribeKey: 'sub-c-8b9161d0-391a-11e8-9da7-9e748936d455'
         });
+
         this.pubnub.init(this);
     }
  
     componentWillMount() {
         this.pubnub.subscribe({
-            channels: ['msgsGPSCoordinates','msgsMessages','msgsTimestamps'],
+            channels: ['messageChannel'],
             withPresence: true
         });
 
-        //  this.pubnub.subscribe({
-        //     channels: ['channel2'],
-        //     withPresence: true
-        // });
- 
-        this.pubnub.getMessage('msgsGPSCoordinates', (msg) => {
-            this.setState({
-                msgsGPSCoordinates: [...this.state.msgsGPSCoordinates,msg.message]
-            });
-            console.log('msgsGPSCoordinates', (msg) => {
-                console.log(msg);
-            });
+       
+
+        this.pubnub.getMessage('messageChannel', (msg) => {
+            // this.setState({
+            //     Messages: [...this.state.Messages,msg]
+            // })
+            console.log(msg);
         });
 
-        this.pubnub.getMessage('msgsMessages', (msg) => {
-            this.setState({
-                msgsMessages: [...this.state.msgsMessages,msg.message]
-            })
-        });
-
-        this.pubnub.getMessage('msgsTimestamps', (msg) => {
-            this.setState({
-                msgsTimestamps: [...this.state.msgsTimestamps,msg.message]
-            })
-        });
+       
 
         this.pubnub.getStatus((st) => {
-            this.pubnub.publish({
-                message: {
-                           gps : "here",
-                         },
-                channel: 'msgsGPSCoordinates'
-            });
+            console.log("st is:" + st);
 
             this.pubnub.publish({
-                message: { 
-                            msg : "need help"
-                          },
-                channel: 'msgsMessages'
-            });
-
-             this.pubnub.publish({
-                message: { 
-                            timestamp : "right now"
-                          },
-                channel: 'msgsTimestamps'
+                message: "i am a message",
+                channel: 'messageChannel'
             });
         });
  
@@ -79,44 +50,16 @@ class App extends Component {
  
     componentWillUnmount() {
         this.pubnub.unsubscribe({
-            channels: ['msgsGPSCoordinates','msgsMessages','msgsTimestamps']
+            channels: ['messageChannel']
         });
     }
 
-    publishMessageToChannel(){
-            console.log("hello button was clicked");
-            this.pubnub.publish({
-                message: {
-                           gps : "helloooooo gps",
-                         },
-                channel: 'msgsGPSCoordinates'
-            });
-        
-    }
  
     render() {
-        const msgsGPSCoordinates = this.pubnub.getMessage('msgsGPSCoordinates');
-        const msgsMessages = this.pubnub.getMessage('msgsMessages');
-        const msgsTimestamps = this.pubnub.getMessage('msgsTimestamps');
-
 
         return (
             <div>
-                <ul>
-                    {msgsGPSCoordinates.map((m, index) => <li key={'message' + index}>{m.message.gps}</li>)}
-                </ul>
-
-                <ul>
-                    {msgsMessages.map((m, index) => <li key={'message' + index}>{m.message.msg}</li>)}
-                </ul>
-
-                <ul>
-                    {msgsTimestamps.map((m, index) => <li key={'message' + index}>{m.message.timestamp}</li>)}
-                </ul>
-                <button onClick = {this.publishMessageToChannel}>click</button>
-
-           
-
+                hello
             </div>
         );
   }
